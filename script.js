d2e1e9464455a1b97dfdfd1aad6f817b243e503b
PRASHANT PATHAK <prashant.pathak112@gmail.com>
2025-02-21 18:30:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function showPhone() {
+    const phoneNumber = document.getElementById('phone-number');
+    phoneNumber.style.display = 'inline'; // Show phone number
+    document.querySelector('.click-me').style.display = 'none'; // Hide "Click Me"
+}
